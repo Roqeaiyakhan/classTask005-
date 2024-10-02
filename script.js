a5d7745fad6027b40af078ebdfac20e778e04dff
Roqeaiyakhan <roqaiyajaani@gmail.com>
2024-10-02 20:03:31 +0500

    fully complete

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-function calculateAge() {
-    const birthdate = new Date(document.getElementById("birthdate").value);
-    const today = new Date();
+function calAge() {
+    var birthdate = new Date(document.getElementById("birthdate").value);
+    var today = new Date();
     
     var years = today.getFullYear() - birthdate.getFullYear();
     var months = today.getMonth() - birthdate.getMonth();
     var day =  today.getDay() - birthdate.getDay();
+    
+    if (day < 0) {
+        months--;
+        day += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+    }
 
-}
+    if (months < 0) {
+        years--;
+        months += 12;
+    }
+
+    document.getElementById("years").textContent = years;
+    document.getElementById("months").textContent = months;
+    document.getElementById("day").textContent = day;
+    
+    }
